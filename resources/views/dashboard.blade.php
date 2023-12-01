1846a8824a8d9fee5715b7e01bc4d239f43f05bc
Derick Jan Ong Cantal <derickcantal@users.noreply.github.com>
2023-12-01 16:30:00 +0400

    Update dashboard.blade.php
    sample update commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,8 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
+
+to be changed
         </h2>
     </x-slot>
 
