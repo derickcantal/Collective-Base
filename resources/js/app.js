9289120b0290be7efbd18fff06f4ded858ed8725
Derick Jan Ong Cantal <derickcantal@users.noreply.github.com>
2024-11-18 22:31:02 +0400

    Sync Files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 import 'flowbite';
 import 'flowbite-datepicker';
-import 'flowbite/dist/datepicker.turbo.js';
 
 import Alpine from 'alpinejs';
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,8 +14,8 @@
         <!-- Scripts -->
         <script src={{ asset("build/assets/flowbite/dist/flowbite.min.js") }}"></script>
 
-        <link rel="stylesheet" href="{{ asset("build/assets/app-61dd3f97.css") }}" type='text/css'>
-        <script src="{{ asset("build/assets/app-a81c3a96.js") }}"></script>
+        <link rel="stylesheet" href="{{ asset("build/assets/app-a74d8e4e.css") }}" type='text/css'>
+        <script src="{{ asset("build/assets/app-431b48e2.js") }}"></script>
         
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
