00f04a1687e89804c21e99eba65c252ffa345db8
Derick Jan Ong Cantal <derickcantal@users.noreply.github.com>
2025-06-05 02:57:41 +0400

    Layout

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="{{ asset("build/assets/app-B0LG07WY.css") }}" type='text/css'>
+<link rel="stylesheet" href="{{ asset("build/assets/app-BJfYACIl.css") }}" type='text/css'>
 <script src="{{ asset("build/assets/app-COT6p2Iw.js") }}"></script>
