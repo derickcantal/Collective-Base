feee9487a2876b3e01ed1a70d85fcc3abbeaedfd
Derick Jan Ong Cantal <derickcantal@users.noreply.github.com>
2025-06-05 22:57:31 +0400

    script update

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="{{ asset("build/assets/app-BJfYACIl.css") }}" type='text/css'>
+<link rel="stylesheet" href="{{ asset("build/assets/app-BO8iWMQU.css") }}" type='text/css'>
 <script src="{{ asset("build/assets/app-COT6p2Iw.js") }}"></script>
